45cd6b9a45d2c9d8e286b9b640c9532227a4a1a7
Aaron Meisner <aaron.meisner@noirlab.edu>
2022-04-12 00:38:29 -0700

    first pass at making an actual repository of 'special numbers'

--- py/decam_reduce/common.py
+++ py/decam_reduce/common.py
@@ -6,3 +6,23 @@ Central location for parameters expected to be used in various other places
 throughout the codebase. Goal is to factor out special numbers.
 
 """
+
+def decam_params():
+    """
+    Set of configuration parameters relevant to DECam reductions.
+
+    Returns
+    -------
+        par : dict
+            Dictionary of parameters.
+
+    Notes
+    -----
+        Eventually generalize somehow for other cameras?
+
+    """
+
+    par = {'max_pixel_radius_deg': 1.0923879,
+           'shard_cone_margin_deg': 0.1}
+
+    return par
