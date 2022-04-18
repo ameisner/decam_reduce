d14a738645502f44dc3b02efb0f3ef84bc6f6c73
Aaron Meisner <aaron.meisner@noirlab.edu>
2022-04-17 22:05:05 -0700

    all one-liners now...

--- bin/2018-09-05_multiproc.sh
+++ bin/2018-09-05_multiproc.sh
@@ -1,3 +1,1 @@
-PROC_1NIGHT_PATH=$(./proc_1night_path.sh)
-
-python -u $PROC_1NIGHT_PATH 2018-09-05 --limit 1 --multiproc 10 &> 2018-09-05_multiproc.log &
+python -u $(./proc_1night_path.sh) 2018-09-05 --limit 1 --multiproc 10 &> 2018-09-05_multiproc.log &
--- bin/2018-09-05_ps1-download.sh
+++ bin/2018-09-05_ps1-download.sh
@@ -1,3 +1,1 @@
-PROC_1NIGHT_PATH=$(./proc_1night_path.sh)
-
-python -u $PROC_1NIGHT_PATH 2018-09-05 --limit 1 --do_ps1_download &> 2018-09-05_ps1-download.log &
+python -u $(./proc_1night_path.sh) 2018-09-05 --limit 1 --do_ps1_download &> 2018-09-05_ps1-download.log &
