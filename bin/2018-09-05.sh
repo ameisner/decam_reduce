99eb5a5c3051c6423837f572649580049088705c
Aaron Meisner <aaron.meisner@noirlab.edu>
2022-04-17 21:52:37 -0700

    standalone script to retrieve the path to deca_reduce.proc_1night, to avoid replication

--- bin/2018-09-05.sh
+++ bin/2018-09-05.sh
@@ -1,3 +1,3 @@
-PROC_1NIGHT_PATH=$(echo -e import decam_reduce.proc_1night "\n"print\(decam_reduce.proc_1night.__file__\) |python)
+PROC_1NIGHT_PATH=$(./proc_1night_path.sh)
 
 python -u $PROC_1NIGHT_PATH 2018-09-05 --limit 1 &> 2018-09-05.log &
---
+++ bin/proc_1night_path.sh
@@ -0,0 +1,2 @@
+PROC_1NIGHT_PATH=$(echo -e import decam_reduce.proc_1night "\n"print\(decam_reduce.proc_1night.__file__\) |python)
+echo $PROC_1NIGHT_PATH
