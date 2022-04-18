02710cba4946e6beb81fb503921dcb2834a41156
Aaron Meisner <aaron.meisner@noirlab.edu>
2022-04-17 21:30:32 -0700

    have shell script figure out the path to the decam_reduce.proc_1night script

--- bin/2018-09-05.sh
+++ bin/2018-09-05.sh
@@ -1,1 +1,3 @@
-python -u ../py/decam_reduce/proc_1night.py 2018-09-05 --limit 1 &> 2018-09-05.log &
+PROC_1NIGHT_PATH=$(echo -e import decam_reduce.proc_1night "\n"print\(decam_reduce.proc_1night.__file__\) |python)
+
+python -u $PROC_1NIGHT_PATH 2018-09-05 --limit 1 &> 2018-09-05.log &
